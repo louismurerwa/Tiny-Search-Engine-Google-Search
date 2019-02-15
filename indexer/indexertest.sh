5ac20f37e130cdfcedd49fffb0220f0621955a02
Louis B. Murerwa '21 <louis@flume.cs.dartmouth.edu>
2019-02-15 14:42:42 -0500

    Index Script

--- indexer/indexertest.sh
+++ indexer/indexertest.sh
@@ -21,7 +21,9 @@ echo "[" `date` "] Build successful!" >> testoutput.txt
 echo "Test 1: Run with a bad/non-existent directory" >> testoutput.txt
 echo "Command: ../indexer ../crawler/fjvioriorio  output" >> testoutput.txt
 
-./indexer ../crawler/fjvioriorio  output &> testoutput.txt
+echo "" >> testoutput.txt
+
+./indexer ../crawler/fjvioriorio  output &>> testoutput.txt
 
 
 echo "" >> testoutput.txt
@@ -30,7 +32,9 @@ echo "" >> testoutput.txt
 echo "Test #2: Running with a nonexistent target directory" >> testoutput.txt
 echo "Command: ./indexer dlkdjfaslkdjf test_output.dat" >> testoutput.txt
 
-./indexer  &> testoutput.txt
+echo "" >> testoutput.txt
+
+./indexer  &>> testoutput.txt
 
 echo "" >> testoutput.txt
 echo "" >> testoutput.txt
@@ -39,15 +43,19 @@ echo "" >> testoutput.txt
 echo "Test #3:Run one arguments" >> testoutput.txt
 echo "Command: ./indexer ../crawler/testDir " >> testoutput.txt
 
-./indexer ../crawler/testDir  &> testoutput.txt
+echo "" >> testoutput.txt
+
+./indexer ../crawler/testDir  &>> testoutput.txt
 
 echo "" >> testoutput.txt
 echo "" >> testoutput.txt
 
-echo "Test #4:Run three arguments" &> testoutput.txt
+echo "Test #4:Run three arguments" >> testoutput.txt
 echo "Command: ./indexer ../crawler/testDir output input" >> testoutput.txt
 
-./indexer ../crawler/testDir output input >> testoutput.txt
+echo "" >> testoutput.txt
+
+./indexer ../crawler/testDir output input &>> testoutput.txt
 
 echo "" >> testoutput.txt
 echo "" >> testoutput.txt
@@ -56,7 +64,10 @@ echo "Test #5: Running with two valid arguments" >> testoutput.txt
 echo "Expected output: a complete index file named output" >> testoutput.txt
 echo "Command: ./indexer ../crawler/testDir output" >> testoutput.txt
 
-./indexer ../crawler/testDir output &> testoutput.txt
+echo "" >> testoutput.txt
+
+./indexer ../crawler/testDir output &>> testoutput.txt
+
 echo "" >> testoutput.txt
 echo "" >> testoutput.txt
 
@@ -65,7 +76,8 @@ echo "Testing the indexertest.c program" >> testoutput.txt
 echo "Test #6: Run with few arguments" >> testoutput.txt
 echo "Command: ./indextest output" >> testoutput.txt
 
-./indextest output >> testoutput.txt
+echo "" >> testoutput.txt
+./indextest output &>> testoutput.txt
 
 echo "" >> testoutput.txt
 echo "" >> testoutput.txt
@@ -73,7 +85,8 @@ echo "" >> testoutput.txt
 echo "Test #7: Run with incorrect file index datastructure" >> testoutput.txt
 echo "Command: ./indextest output1 newoutput" >> testoutput.txt
 
-./indextest output1 newoutput >> testoutput.txt
+echo "" >> testoutput.txt
+./indextest output1 newoutput &>> testoutput.txt
 
 echo "" >> testoutput.txt
 echo "" >> testoutput.txt
@@ -82,7 +95,8 @@ echo "" >> testoutput.txt
 echo "Test #8:Running with correct arguments" >> testoutput.txt
 echo "Command: ./indextest output  newoutput" >> testoutput.txt
 
-./indextest output newoutput >> testoutput.txt
+echo "" >> testoutput.txt
+./indextest output newoutput &>> testoutput.txt
 
 echo "" >> testoutput.txt
 echo "" >> testoutput.txt
